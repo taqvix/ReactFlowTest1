3c49ab8826d607f0979f2ef0e51d02abc4c18cc4
taqvix <taqvix@pm.me>
2024-07-16 09:42:43 +0530

    addingColor

--- src/App.tsx
+++ src/App.tsx
@@ -13,10 +13,14 @@ import {
 import "@xyflow/react/dist/style.css";
 
 const initialNodes = [
-  { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
-  { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
+  { id: "1", position: { x: 0, y: 0 }, data: { label: "Box1" } },
+  { id: "2", position: { x: -50, y: 100 }, data: { label: "Box2" } },
+  { id: "3", position: { x: 50, y: 100 }, data: { label: "Box3" } },
 ];
-const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
+const initialEdges = [
+  { id: "e1-2", source: "1", target: "2", animated: true },
+  { id: "e1-3", source: "1", target: "3", style: { stroke: "#c2410c" } },
+];
 
 export default function App() {
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
